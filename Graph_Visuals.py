bde3b7ed2b18285207778edd5ddab53efef4ffdd
shillis17 <shillis17@live.com>
2019-05-28 15:15:25 -0600

    New dataframes to graph from.

--- Graph_Visuals.py
+++ Graph_Visuals.py
@@ -11,7 +11,8 @@ df_genre = df.drop(['Unnamed: 0', 'QueryName', 'ReleaseDate', 'RequiredAge', 'DL
        'CategoryInAppPurchase', 'CategoryIncludeLevelEditor',\
        'CategoryVRSupport','PriceCurrency', 'PriceInitial', 'PriceFinal', 'Reviews'],axis=1)
 
-data = {'action' : df_genre['GenreIsAction'].sum(),
+data = {'indie' : df_genre['GenreIsIndie'].sum(),
+'action' : df_genre['GenreIsAction'].sum(),
 'adventure' : df_genre['GenreIsAdventure'].sum(),
 'casual' : df_genre['GenreIsCasual'].sum(),
 'strategy' : df_genre['GenreIsStrategy'].sum(),
@@ -23,7 +24,9 @@ data = {'action' : df_genre['GenreIsAction'].sum(),
 'mmo' : df_genre['GenreIsMassivelyMultiplayer'].sum(),
 'ftp' : df_genre['GenreIsFreeToPlay'].sum()}
 
-fig1, axs = plt.subplots(figsize=(16,14))
+
+
+fig, axs = plt.subplots(figsize=(16,14))
 plt.title('Genre Frequency')
 names = list(data.keys())
 values = list(data.values())
@@ -31,10 +34,25 @@ axs.bar(names,values)
 plt.xticks(rotation=90)
 plt.savefig('images/GenreFrequencyBar.png')
 
-fig1, axs = plt.subplots(figsize=(14,14))
+fig, axs = plt.subplots(figsize=(14,14))
 plt.title('Genre Proportions')
 names = list(data.keys())
 values = list(data.values())
 axs.pie(values)
 axs.legend(loc='right',bbox_to_anchor=(1.05, .5),labels=names)
 plt.savefig('images/GenreFrequencyPie.png')
+
+
+df_indie = df[df.GenreIsIndie == True]
+df_action = df[df.GenreIsAction == True]
+df_adventure = df[df.GenreIsAdventure == True]
+df_casual = df[df.GenreIsCasual == True]
+df_stratrgy = df[df.GenreIsStrategy == True]
+df_simulation = df[df.GenreIsSimulation == True]
+df_rpg = df[df.GenreIsRPG == True]
+df_earlyaccess = df[df.GenreIsEarlyAccess == True]
+df_sports = df[df.GenreIsSports == True]
+df_racing = df[df.GenreIsRacing == True]
+df_mmo = df[df.GenreIsMassivelyMultiplayer == True]
+df_ftp = df[df.GenreIsFreeToPlay == True]
+
